46266d47a23ed269482a07cdae52f320607391c2
Samuel Bouye <bouyesamuel@gmail.com>
2020-05-21 14:13:25 +0000

    restart

--- config/db.js
+++ config/db.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize')
 
-const sequelize = new Sequelize('progwrld-db', 'root','root', {
+const sequelize = new Sequelize('progwrld_db', 'root','root', {
     host: 'localhost',
     dialect: 'mysql',
 })
